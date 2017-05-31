ddef1a59d7f84a5715b6ebdb76604e6d101fef6a
Eduardo Hattori <eduardo.hattori@hotmail.com>
2017-05-31 11:09:53 -0300

    Config enviroments variables

--- manage.py
+++ manage.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 import os
 import sys
+import pymysql
+
+
+pymysql.install_as_MySQLdb()
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.config.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
